dc0c2b6bf24ed50728b5ecb7b8696277d3dda384
Muhammad Rasikh Riaz <mo.rasikhriaz@gmail.com>
2025-07-31 22:04:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, Kubernetes!! Applications'));
+app.get('/', (req, res) => res.send('Hello, Kubernetes!! Applications RASIKH'));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
